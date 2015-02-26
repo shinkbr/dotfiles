26d1a079af844250f3f1ca990f06870d3a1051a2
Shintaro Kobori <shinkbr@gmail.com>
2015-02-26 21:34:13 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dotdir=$(dirname $0)
+
+declare -a files=(
+zshrc
+zsh-syntax-highlighting
+vim
+vimrc
+tmux.conf
+gitconfig
+gitignore
+)
+
+red='\033[31m'
+nocolor='\033[0m'
+
+for i in ${files[@]}
+do
+  if [ ! -e ~/.$i ]
+  then
+    echo "ln -s $dotdir/$i ~/.$i"
+    ln -s $dotdir/$i ~/.$i
+  else
+    echo -e "${red}~/.$i exists.${nocolor}"
+  fi
+done
+
+echo ""
+echo "You should also symbolic-link your zshenv file as necessary."
