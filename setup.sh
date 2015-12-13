b41cc5cc9dae807d916a0eea659c747754d41906
Shintaro Kobori <shinkbr@gmail.com>
2015-12-14 08:46:43 +0900

    add neobundle.vim setup

--- setup.sh
+++ setup.sh
@@ -26,5 +26,10 @@ do
   fi
 done
 
+# neobundle.vim
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim && cd ~/.vim/bundle/vimproc.vim && make
+
 echo ""
 echo "You should also symbolic-link your zshenv file as necessary."
